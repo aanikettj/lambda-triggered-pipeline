e6c61531ad8628dfbd5536e4f0ac5e78a846e62e
Aniket Balasaheb Jadhav <niketjadhav7007@gmail.com>
2026-02-17 17:36:50 +0530

    Update lambda-function.py

--- lambda-function.py
+++ lambda-function.py
@@ -5,8 +5,8 @@ def lambda_handler(event, context):
 
     jenkins_url = "http://18.212.86.180:8080/job/lambda-triggered-pipeline/build"
 
-    username = "admin"
-    api_token = "112f3b4130fad0573d107ef75d3e49313a"
+    username = "jenkins username"
+    api_token = "jenkins token"
 
     auth = base64.b64encode(f"{username}:{api_token}".encode()).decode()
 
